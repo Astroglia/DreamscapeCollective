3cc8af6d338e8ec425c20b2286b1f54a5981d48b
Astroglia <42473606+Astroglia@users.noreply.github.com>
2020-07-25 21:06:50 -0500

    initial dream tests conducted

--- DreamCollective.py
+++ DreamCollective.py
@@ -1,5 +1,8 @@
 #main file
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 import numpy as np
 from Dreamer import Dream, DreamExtractor
 
@@ -8,5 +11,14 @@ dream_loader = DreamExtractor(dream_file='./dreams/badea/Badea2011Brn3aONandOFF-
 dream_loader.read_dream()
 generated_dream = dream_loader.generate_dream()
 
-new_dream = Dream( generated_dream.dreamtree )
-new_dream.see_dream()
+new_dream = Dream( generated_dream.dreamtree, generated_dream.raw_point_data )
+
+fig = plt.figure()
+
+data = np.dstack(new_dream.raw_point_data)
+data = data[0, ...]
+print(data.shape)
+
+ax = fig.add_subplot( 111 , projection='3d')
+ax.scatter(data[0, :], data[1, :], data[2, :], c='r', marker='o')
+plt.show() 
